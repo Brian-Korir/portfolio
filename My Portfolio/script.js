e92a33199754fb53ce4f740b24273c291b401e8b
Brian Korir <144766290+Brian-Korir@users.noreply.github.com>
2024-05-16 23:18:00 +0300

    Update script.js

--- My Portfolio/script.js
+++ My Portfolio/script.js
@@ -3,7 +3,8 @@ const menu = document.queryselector(".menu-links");
   const icon = document.queryselector(".hamburger-icon");
   menu.classlist.toggle("open");
 icon.classlist.toggle("open); 
-window.alert("Welcome 🤗")
+window.alert("Welcome 🤗");
+
                     
  }
 
