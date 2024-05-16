f005c235b42342c5df45e09a728058a3cff5ae93
Brian Korir <144766290+Brian-Korir@users.noreply.github.com>
2024-05-16 23:16:46 +0300

    Update script.js

--- My Portfolio/script.js
+++ My Portfolio/script.js
@@ -2,6 +2,7 @@ function toggleMenu() {
 const menu = document.queryselector(".menu-links");
   const icon = document.queryselector(".hamburger-icon");
   menu.classlist.toggle("open");
-icon.classlist.toggle("open);                      
+icon.classlist.toggle("open);  
+                    
  }
 
